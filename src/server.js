f5a42940c0f80770dec7e11d97e69f7dd9c713f7
Nuwan Sampath <nuwanliyanamana1996@gmail.com>
2026-01-10 19:44:54 +0530

    issue fixed

--- src/server.js
+++ src/server.js
@@ -131,12 +131,14 @@ app.use((err, req, res, next) => {
 //     process.exit(0);
 // });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || '127.0.0.1';
+
 async function startServer() {
     try {
     //   await connectDB();
   
-      server.listen(PORT, () => {
+      server.listen(PORT, HOST,() => {
         console.log(`Server running on port ${PORT}`);
       });
     } catch (err) {
